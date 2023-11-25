09c1efdd53c221c4993705240f52c89bc8751293
Marija Kuveljic <kuveljic117@gmail.com>
2023-11-25 19:18:14 +0100

    Create product table and seeder.

---
+++ database/seeders/ProductsTableSeeder.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Sample data for products
+        $products = [
+            [
+                'name' => 'Naturela sa Rogačem i Agava Šećerom',
+                'size' => '750g',
+                'price' => 746,
+                'discounted_price' => 546,
+                'image' => 'product1.jpg',
+            ],
+            [
+                'name' => 'Naturela sa Rogačem i Agava Šećerom',
+                'size' => '750g',
+                'price' => 546,
+                'image' => 'product2.jpg',
+            ],
+            [
+                'name' => 'Naturela sa Rogačem i Agava Šećerom',
+                'size' => '750g',
+                'price' => 546,
+                'image' => 'product2.jpg',
+            ],
+            [
+                'name' => 'Naturela sa Rogačem i Agava Šećerom',
+                'size' => '750g',
+                'price' => 746,
+                'discounted_price' => 546,
+                'image' => 'product3.jpg',
+            ],
+            [
+                'name' => 'Naturela sa Rogačem i Agava Šećerom',
+                'size' => '750g',
+                'price' => 546,
+                'image' => 'product2.jpg',
+            ],
+            [
+                'name' => 'Naturela sa Rogačem i Agava Šećerom',
+                'size' => '750g',
+                'price' => 546,
+                'image' => 'product2.jpg',
+            ],
+            [
+                'name' => 'Naturela sa Rogačem i Agava Šećerom',
+                'size' => '750g',
+                'price' => 546,
+                'image' => 'product4.jpg',
+            ],
+            [
+                'name' => 'Naturela sa Rogačem i Agava Šećerom',
+                'size' => '750g',
+                'price' => 746,
+                'discounted_price' => 546,
+                'image' => 'product6.jpg',
+            ],
+            [
+                'name' => 'Naturela sa Rogačem i Agava Šećerom',
+                'size' => '750g',
+                'price' => 546,
+                'image' => 'product2.jpg',
+            ],
+        ];
+    }
+}
