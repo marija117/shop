f8ebf3cc20d46c274324175785fe98bc4256140c
Marija Kuveljic <kuveljic117@gmail.com>
2023-11-26 21:09:14 +0100

    Update index page according to design.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,16 +6,16 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 text-gray-200" />
+                        <img class="object-cover w-auto fill-current " src="{{ asset('storage/logo_builtt_veci.png') }}" alt="logo">
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                <!-- <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('Builtt') }}
                     </x-nav-link>
-                </div>
+                </div> -->
             </div>
 
             <!-- Settings Dropdown -->
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -2,21 +2,42 @@
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 text-gray-200 leading-tight">
             {{ __('Svi proizvodi') }}
+            <span class="text-gray-400 text-sm font-normal">
+                {{ $products->count() }}{{ __(' proizvoda') }}
+            </span>
         </h2>
     </x-slot>
 
         <div class="container mx-auto p-8">
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
             @foreach($products as $product)
-                <div class="max-w-sm rounded overflow-hidden shadow-lg bg-gray-100">
-                    <img class="w-full" src="{{ asset($product->image) }}" alt="{{ $product->name }}">
-                    <div class="px-6 py-4">
-                        <div class="font-bold text-xl mb-2">{{ $product->name }}</div>
-                        <p class="text-gray-700 text-base">{{ $product->description }}</p>
-                        <p class="text-gray-900 text-base">{{ $product->price }} RSD</p>
-                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">
-                            Add to Cart
+                <div class="max-w-sm overflow-hidden">
+                    <div class="relative">
+                        <img class="object-cover w-full" src="{{ asset($product->image) }}" alt="{{ $product->name }}">
+                        <button class="absolute bottom-2 left-2 px-2 py-2 bg-white text-gray-700 rounded-l-2xl focus:outline-none" onclick="decrementCounter(this)">
+                            <!-- Minus Icon -->
+                            <svg class="h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M20 12H4"></path>
+                            </svg>
                         </button>
+                        <span id="counter" class="absolute bottom-2 left-10 bg-white px-2 py-1  text-gray-700">1</span>
+                        <button class="absolute bottom-2 left-14 px-2 py-2 bg-white text-gray-700 rounded-r-2xl focus:outline-none" onclick="incrementCounter(this)">
+                            <!-- Plus Icon -->
+                            <svg class="h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
+                            </svg>
+                        </button>
+                        <button class="absolute bottom-3 left-24 p-1 bg-black text-white rounded-full">
+                            <img src="{{ asset('storage/cart.png') }}" alt="cart">
+                        </button>
+                    </div>
+                    <div class="px-6 py-4">
+                        <div class="font-bold text-l mb-2">{{ $product->name }}</div>
+                        <p class="font-semibold text-xl">{{ $product->price }} RSD</p>
                     </div>
                 </div>
             @endforeach
@@ -24,3 +45,17 @@
     </div>
 
 </x-app-layout>
+<script>
+    function incrementCounter(button) {
+        var counterElement = button.parentNode.querySelector("#counter");
+        counterElement.textContent = parseInt(counterElement.textContent) + 1;
+    }
+
+    function decrementCounter(button) {
+        var counterElement = button.parentNode.querySelector("#counter");
+        var currentCount = parseInt(counterElement.textContent);
+        if (currentCount > 1) {
+            counterElement.textContent = currentCount - 1;
+        }
+    }
+</script>
